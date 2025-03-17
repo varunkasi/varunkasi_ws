213cdbf02bfbba1e67f13c41f492e0d23e8ab773
varunkasi <vkasired@alumni.cmu.edu>
2025-03-17 01:00:07 -0400

    Remove build, install, and log directories from version control

--- install/thermal_calibration_tool/lib/python3.10/site-packages/tests/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Test package for the Thermal Camera Calibration Tool.
-
-This package contains test modules for unit testing the application components.
-"""
